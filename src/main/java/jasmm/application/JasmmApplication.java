8810d9eb7b4eaf509c7550c048f35aac84ae3b70
Science4tw <matthias.wyss.mw@live.de>
2022-03-05 23:00:58 +0100

    First commit

---
+++ src/main/java/jasmm/application/JasmmApplication.java
@@ -0,0 +1,13 @@
+package jasmm.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jasmm/application/JasmmApplicationTests.java
@@ -0,0 +1,13 @@
+package jasmm.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
