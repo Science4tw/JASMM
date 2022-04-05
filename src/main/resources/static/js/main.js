cfbb142d0f3ce28ed7f34413dfaf32d5b763a392
miche <miche@192.168.0.20>
2022-04-05 10:37:24 +0200

    Klassen aus Mockups integriert (html, js, css)

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,40 @@
+// USER FORM SCRIPT
+
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const usernameInput = document.querySelector('#username');
+const pwdInput = document.querySelector('#pwd');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(usernameInput.value === '' || pwdInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${usernameInput.value}: ${pwdInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
+
+    // Clear fields
+    usernameInput.value = '';
+    pwdInput.value = '';
+  }
+}
