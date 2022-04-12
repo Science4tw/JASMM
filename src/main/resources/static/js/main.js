f1efa5b9b7a0a1848dd8df9b9d870fffa3a879f0
Hotzeeplotz <101449925+Hotzeeplotz@users.noreply.github.com>
2022-04-12 16:47:50 +0200

    EMail Validierung von index.html entfernt.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -215,7 +215,6 @@ function checkEmail(str) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test(str)) {
     alert("Bitte geben Sie eine gueltige E-Mailadresse ein!");
-    document.getElementById("username").value="";
 	document.getElementById("name").value="";
 	}
     
